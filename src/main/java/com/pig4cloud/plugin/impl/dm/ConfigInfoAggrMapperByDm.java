9308bc052b9ab9e824bf123caa8600bbd08419f8
冷冷 <2270033969@qq.com>
2025-01-29 22:37:18 +0800

    upgrade(nacos): 2.5.0

--- src/main/java/com/pig4cloud/plugin/impl/dm/ConfigInfoAggrMapperByDm.java
+++ src/main/java/com/pig4cloud/plugin/impl/dm/ConfigInfoAggrMapperByDm.java
@@ -1,33 +1,34 @@
 package com.pig4cloud.plugin.impl.dm;
 
-import com.alibaba.nacos.common.utils.CollectionUtils;
-import com.alibaba.nacos.plugin.datasource.constants.FieldConstant;
-import com.alibaba.nacos.plugin.datasource.mapper.ConfigInfoAggrMapper;
+import com.alibaba.nacos.plugin.datasource.mapper.ConfigInfoGrayMapper;
 import com.alibaba.nacos.plugin.datasource.model.MapperContext;
 import com.alibaba.nacos.plugin.datasource.model.MapperResult;
 import com.pig4cloud.plugin.constants.DataSourceConstant;
 
-import java.util.List;
+import java.util.Collections;
 
-public class ConfigInfoAggrMapperByDm extends DmAbstractMapper implements ConfigInfoAggrMapper {
+public class ConfigInfoAggrMapperByDm extends DmAbstractMapper implements ConfigInfoGrayMapper {
 
-	@Override
-	public MapperResult findConfigInfoAggrByPageFetchRows(MapperContext context) {
-		int startRow = context.getStartRow();
-		int pageSize = context.getPageSize();
-		String dataId = (String) context.getWhereParameter(FieldConstant.DATA_ID);
-		String groupId = (String) context.getWhereParameter(FieldConstant.GROUP_ID);
-		String tenantId = (String) context.getWhereParameter(FieldConstant.TENANT_ID);
 
-		String sql = "SELECT data_id,group_id,tenant_id,datum_id,app_name,content FROM config_info_aggr WHERE data_id= ? AND "
-				+ "group_id= ? AND tenant_id= ? ORDER BY datum_id LIMIT " + startRow + "," + pageSize;
-		List<Object> paramList = CollectionUtils.list(dataId, groupId, tenantId);
-		return new MapperResult(sql, paramList);
-	}
+    @Override
+    public MapperResult updateConfigInfo4GrayCas(MapperContext context) {
+        return ConfigInfoGrayMapper.super.updateConfigInfo4GrayCas(context);
+    }
 
-	@Override
-	public String getDataSource() {
-		return DataSourceConstant.DM;
-	}
+    @Override
+    public MapperResult findChangeConfig(MapperContext context) {
+        return ConfigInfoGrayMapper.super.findChangeConfig(context);
+    }
+
+    @Override
+    public MapperResult findAllConfigInfoGrayForDumpAllFetchRows(MapperContext context) {
+        String sql = " SELECT id,data_id,group_id,tenant_id,gray_name,gray_rule,app_name,content,md5,gmt_modified  FROM  config_info_gray  ORDER BY id LIMIT " + context.getStartRow() + "," + context.getPageSize();
+        return new MapperResult(sql, Collections.emptyList());
+    }
+
+    @Override
+    public String getDataSource() {
+        return DataSourceConstant.DM;
+    }
 
 }
