e112b755c698563d5f34e106e061264dddf7d1ac
冷冷 <2270033969@qq.com>
2025-01-29 23:01:57 +0800

    refactor: Improve ConfigInfoAggrMapperByDm code formatting

--- src/main/java/com/pig4cloud/plugin/impl/dm/ConfigInfoAggrMapperByDm.java
+++ src/main/java/com/pig4cloud/plugin/impl/dm/ConfigInfoAggrMapperByDm.java
@@ -9,26 +9,26 @@ import java.util.Collections;
 
 public class ConfigInfoAggrMapperByDm extends DmAbstractMapper implements ConfigInfoGrayMapper {
 
+	@Override
+	public MapperResult updateConfigInfo4GrayCas(MapperContext context) {
+		return ConfigInfoGrayMapper.super.updateConfigInfo4GrayCas(context);
+	}
 
-    @Override
-    public MapperResult updateConfigInfo4GrayCas(MapperContext context) {
-        return ConfigInfoGrayMapper.super.updateConfigInfo4GrayCas(context);
-    }
+	@Override
+	public MapperResult findChangeConfig(MapperContext context) {
+		return ConfigInfoGrayMapper.super.findChangeConfig(context);
+	}
 
-    @Override
-    public MapperResult findChangeConfig(MapperContext context) {
-        return ConfigInfoGrayMapper.super.findChangeConfig(context);
-    }
+	@Override
+	public MapperResult findAllConfigInfoGrayForDumpAllFetchRows(MapperContext context) {
+		String sql = " SELECT id,data_id,group_id,tenant_id,gray_name,gray_rule,app_name,content,md5,gmt_modified  FROM  config_info_gray  ORDER BY id LIMIT "
+				+ context.getStartRow() + "," + context.getPageSize();
+		return new MapperResult(sql, Collections.emptyList());
+	}
 
-    @Override
-    public MapperResult findAllConfigInfoGrayForDumpAllFetchRows(MapperContext context) {
-        String sql = " SELECT id,data_id,group_id,tenant_id,gray_name,gray_rule,app_name,content,md5,gmt_modified  FROM  config_info_gray  ORDER BY id LIMIT " + context.getStartRow() + "," + context.getPageSize();
-        return new MapperResult(sql, Collections.emptyList());
-    }
-
-    @Override
-    public String getDataSource() {
-        return DataSourceConstant.DM;
-    }
+	@Override
+	public String getDataSource() {
+		return DataSourceConstant.DM;
+	}
 
 }
